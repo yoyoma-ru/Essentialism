dff4c574bfded0f353fcf8ed5ee4cb755fee2cab
vagrant <asjstksmr@gmail.com>
2020-09-12 02:30:06 +0000

    [fix]修正

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -26,9 +26,3 @@ $(function(){
 	});
 });
 
-$(function(){
-	$('.linkInThePage').click( function () {
-		var hrefValue = jQuery(this).attr( 'href' );
-		$(hrefValue).animatescroll({padding:70});
-	} );
-} );
