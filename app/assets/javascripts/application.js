4bb06703886b3b553b4647b2e2b4554f2785ed18
vagrant <asjstksmr@gmail.com>
2020-09-20 19:50:59 +0000

    [add]learn/indexページ

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -26,3 +26,9 @@ $(function(){
 	});
 });
 
+$(function(){
+	$('.linkInThePage').click( function () {
+		var hrefValue = jQuery(this).attr( 'href' );
+		$(hrefValue).animatescroll({padding:70},800);
+	} );
+} );
