e17a1c43204b432c633a05f4f5ba551afd38d983
愿山聖 <asjstksmr@gmail.com>
2020-10-05 12:24:29 +0900

    [fix]my_page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,40 @@
 class UsersController < ApplicationController
+
+  before_action :authenticate_user!
+
   def show
   	@user = User.find(params[:id])
   end
 
   def edit
   	@user = User.find(params[:id])
+  	if @user == current_user
+  		render :edit
+  	else
+  		redirect_to user_path(@user)
+  	end
+  end
+
+  def update
+  	@user = User.find(params[:id])
+  	if @user.update(user_params)
+  		redirect_to user_path(@user)
+  	else
+  		render :edit
+  	end
+  end
+
+  def destroy
+  	@user = User.find(params[:id])
+  	if @user.destroy
+  		redirect_to root_path
+  	else
+  		render :show
+  	end
+  end
+
+  private
+  def user_params
+  	params.require(:user).permit(:name, :profile_image, :introduction)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users, only: [:show]
+  resources :users, only: [:show, :edit, :update,:destroy]
 
-  get '/' => 'homes#top'
+  root 'homes#top'
   get '/about' => 'homes#about'
 
 
-  get '/learns' => 'learns#index'
+  get '/learns' => 'learns#index', as: "learns"
   get '/learns/chapter1' => 'learns#chapter1'
   get '/learns/chapter2' => 'learns#chapter2'
   get '/learns/chapter3' => 'learns#chapter3'
@@ -28,7 +28,7 @@ Rails.application.routes.draw do
   get '/learns/chapter18' => 'learns#chapter18'
   get '/learns/chapter19' => 'learns#chapter19'
 
-  get '/works' => 'works#index'
+  get '/works' => 'works#index', as: "works"
   get '/works/step1_selection' => 'works#step1_selection'
   get '/works/step1_noise' => 'works#step1_noise'
   get '/works/step1_tradeoff' => 'works#step1_tradeoff'
@@ -37,5 +37,5 @@ Rails.application.routes.draw do
   get '/works/step3_necessary_actions' => 'works#step3_necessary_actions'
   get '/works/step4_baby_steps' => 'works#step4_baby_steps'
   get '/works/step4_habits' => 'works#step4_habits'
-  get '/works/user_works' => 'works#user_works'
+  get '/works/user_works' => 'works#user_works', as: "user_works"
 end
