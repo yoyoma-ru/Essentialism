906219a8dad9c4a4840b9149d67834600a0f60d4
愿山聖 <asjstksmr@gmail.com>
2020-12-22 23:32:20 +0900

    [fix]index

---
+++ app/assets/javascripts/learns.js
@@ -0,0 +1,13 @@
+// 円の大きさを調整
+// $(document).ready(function () {
+//     wsize = $(window).width();
+//     if (wsize < 535 ){
+//     	$(".learn-circle").addClass(".sm-circle");
+//     };
+// });
+// $(window).ready(function () {
+//     wsize = $(window).width();
+//     if (wsize < 535 ){
+//     	$(".learn-circle").addClass(".sm-circle");
+//     };
+// });
