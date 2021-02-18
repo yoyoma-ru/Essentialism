e85c8a6c62495fbf773c4524255e5b013d5e2c80
愿山聖 <asjstksmr@gmail.com>
2021-02-18 10:24:36 +0900

    [fix]top/about

--- app/assets/javascripts/top-page.js
+++ app/assets/javascripts/top-page.js
@@ -1,11 +1,12 @@
-onload = function(){
-	// ↓top.html
+$(document).on('turbolinks:load',function(){
+	// $(".footer").hide();
+	// ↓top.html 写真の遷移
 	const images = document.querySelectorAll(".observer-box");
 	console.log(images);
-
 	const options = {
 		root: null,
 		rootMargin: "0px 0px -90% 0px",
+		// rootMargin: "-50% 0px",
 		threshold: 0
 	};
 	const observer = new IntersectionObserver(
@@ -30,7 +31,15 @@ onload = function(){
 	boxes.forEach(box =>{
 		aboutObserver.observe(box);
 	});
-}
+
+	ScrollReveal().reveal(".about-box",{
+		delay: 300,
+		duration: 2000,
+		opacity: 0.02,
+		viewFactor: 0.2
+	});
+});
+
 
 // ↓top.html
 function doWhenIntersect(entries){
@@ -39,6 +48,9 @@ function doWhenIntersect(entries){
 		if(entry.isIntersecting){
 			console.log(entry.target);
 			activateImage(entry.target);
+			// スクロールの停止
+			no_scroll();
+			setTimeout(return_scroll, 3500);
 		}
 	});
 }
@@ -48,7 +60,7 @@ function activateImage(element){
 		"#image-frame .is-leave");
 	if(currentLeaveImage !== null){
 		currentLeaveImage.classList.remove("is-leave");
-		console.log("leave");
+		console.log("imageのleaveをremoveする");
 	}
 
 	const currentActiveIndex = document.querySelector(
@@ -57,15 +69,14 @@ function activateImage(element){
 	if(currentActiveIndex !== null){
 		currentActiveIndex.classList.remove("is-active");
 		currentActiveIndex.classList.add("is-leave");
-		console.log("image-add image-remove");
+		console.log("前のimageのactiveをremoveしてleaveをつける");
 	}
 
 	const currentActiveText = $("#image-frame").find(".is-active2");
 	console.log(currentActiveText[0]);
 	if(currentActiveText.length !== 0){
 		currentActiveText[0].classList.remove("is-active2");
-		// currentActiveText[0].classList.add(element.id);
-		console.log("text remove");
+		console.log("テキストのactiveをremove");
 	}
 
 	const newActiveImage = document.querySelector(
@@ -73,10 +84,30 @@ function activateImage(element){
 	);
 	console.log(newActiveImage)
 	newActiveImage.classList.add("is-active");
+	console.log("imageにactiveをつける");
 
 	const newActiveText = $(newActiveImage).find(".text-box");
 	console.log(newActiveText);
 	newActiveText[0].classList.add("is-active2");
+	console.log("テキストにactiveをつける");
+}
+// スクロール禁止
+function no_scroll() {
+    // PCでのスクロール禁止
+    document.addEventListener("mousewheel", scroll_control, { passive: false });
+    // スマホでのタッチ操作でのスクロール禁止
+    document.addEventListener("touchmove", scroll_control, { passive: false });
+}
+// スクロール禁止解除
+function return_scroll() {
+    // PCでのスクロール禁止解除
+    document.removeEventListener("mousewheel", scroll_control, { passive: false });
+    // スマホでのタッチ操作でのスクロール禁止解除
+    document.removeEventListener('touchmove', scroll_control, { passive: false });
+}
+// スクロール関連メソッド
+function scroll_control(event) {
+    event.preventDefault();
 }
 
 
