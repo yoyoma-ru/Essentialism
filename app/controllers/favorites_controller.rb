ead25d7afafd3b6e9e28bb45afee23fc2ac50b24
愿山聖 <asjstksmr@gmail.com>
2021-03-18 14:32:53 +0900

    [fix]

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,8 +1,10 @@
 class FavoritesController < ApplicationController
 
+	PER = 10
+
 	def index
-		@user = current_user
-		@favorites = Favorite.where(user_id: @user.id).all
+		user = current_user
+		@favorites = Favorite.page(params[:page]).per(PER).where(user_id: user.id)
 	end
 
 	def create
